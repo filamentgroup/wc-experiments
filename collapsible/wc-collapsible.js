247be194e5819baaa592347d4ed8a22fb7519a30
Scott Jehl <scottjehl@gmail.com>
2020-06-09 17:21:15 -0400

    update collapsible wc to use our latest pattern

--- collapsible/wc-collapsible.js
+++ collapsible/wc-collapsible.js
@@ -1,21 +1,90 @@
 class Collapsible extends HTMLElement {
-	connectedCallback() {
-		const parent = this;
-		parent.classList.add("collapsible-enhanced");
-		
-		const header = this.querySelector('h2,h3,h4,.collapsible-header');
-		header.classList.add("collapsible-header");
-		header.setAttribute("role", "button");
-		header.setAttribute("tabindex", "0");
-		header.setAttribute("aria-expanded", !parent.hasAttribute("collapsed") );
 
-		const content = this.querySelector('div,p,.collapsible-content');
-		content.classList.add("collapsible-content");
+	constructor(){
+		super();
+		this.headerBtn = this;
+		this.content = this.headerBtn.nextElementSibling;
+		this.contentId = this.content.id || "collapsible_" + new Date().getTime();
+		this.collapsed = false;
+		this.addA11yAttrs();
+		this.bindEvents();
+		this.collapse();
+        this.checkInteractivity();
+	}
 
-		header.addEventListener('click', evt => {
-			parent.toggleAttribute("collapsed");
-			header.setAttribute("aria-expanded", !parent.hasAttribute("collapsed"));
-		});
+	addA11yAttrs(){
+		this.headerBtn.setAttribute( "aria-controls", this.contentId );
+		this.content.id = this.contentId;
+	}
+
+	removeA11yAttrs(){
+		this.content.removeAttribute( "data-hidden" );
+	}
+
+
+	isNonInteractive(){
+		var computedContent = window.getComputedStyle( this.content, null );
+		var computedHeader = window.getComputedStyle( this.headerBtn, null );
+		return computedContent.getPropertyValue( "display" ) !== "none" && computedContent.getPropertyValue( "visibility" ) !== "hidden" && ( computedHeader.getPropertyValue( "cursor" ) === "default" || computedHeader.getPropertyValue( "display" ) === "none" );
+	}
+
+	checkInteractivity(){
+		if( this.isNonInteractive() ){
+			this.removeA11yAttrs();
+		}
+		else{
+			this.setState();
+			this.addA11yAttrs();
+		}
+	}
+
+	expand(){
+		this.headerBtn.setAttribute( "aria-expanded", "true" );
+		this.content.setAttribute( "data-hidden", "false" );
+		this.collapsed = false;
+	}
+
+	collapse(){
+		this.headerBtn.setAttribute( "aria-expanded", "false" );
+		this.content.setAttribute( "data-hidden", "true" );
+		this.collapsed = true;
+	}
+
+	setState(){
+		if( this.collapsed ){
+			this.collapse();
+		}
+		else {
+			this.expand();
+		}
+	}
+
+	toggle(){
+		if( this.collapsed ){
+			this.expand();
+		}
+		else {
+			this.collapse();
+		}
+	}
+
+	bindEvents(){
+		var self = this;
+
+		this.headerBtn.addEventListener( "click", function( e ){
+			e.preventDefault();
+			self.toggle();
+        });
+    
+		var resizepoll;
+		window.addEventListener( "resize", function(){
+			if( resizepoll ){
+				window.clearTimeout( resizepoll );
+			}
+			resizepoll = window.setTimeout( function(){
+				self.checkInteractivity.call( self );
+			}, 150 );
+		} );
 	}
 }
   
